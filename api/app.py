94cf7a435d38b3f80163b3171a2a7d2d28f85dd2
Joseph Schwab <52177033+jschwab012@users.noreply.github.com>
2024-04-12 16:04:10 -0400

    Added blueprints for flask

--- api/app.py
+++ api/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, Response, jsonify
 from flask_cors import CORS
+from calendar_blueprint import calendar
+
 app = Flask(__name__)
+app.register_blueprint(calendar)
 CORS(app)
 
 @app.route('/', methods=["GET"])
@@ -8,7 +11,6 @@ def status():
     return jsonify({
         "status" : "running"
     })
-    # return Response({ "status": "running" })
 
 if __name__ == '__main__':
     app.run(port=5001)
--- frontend/src/components/recommendation/Course.tsx
+++ frontend/src/components/recommendation/Course.tsx
@@ -1,16 +1,23 @@
-import { grid } from "@chakra-ui/react";
+import { grid, SimpleGrid, Box } from "@chakra-ui/react";
 
 export default function Course(props:any) {  
     return (
-        <div style={
-            {
-                height: props.height, 
-                width: props.width, 
-                backgroundColor: props.color,
-                }
-            }
-        >
-        </div>
+        <SimpleGrid columns={5} spacing={2}>
+            <Box bg='tomato' height='200px'></Box>
+            <Box bg='tomato' height='200px'></Box>
+            <Box bg='tomato' height='80px'></Box>
+            <Box bg='tomato' height='80px'></Box>
+            <Box bg='tomato' height='80px'></Box>
+        </SimpleGrid>
+        // <div style={
+        //     {
+        //         height: props.height, 
+        //         width: props.width, 
+        //         backgroundColor: props.color,
+        //         }
+        //     }
+        // >
+        // </div>
         // <p style = {{backgroundColor: props.color}}>Hi!</p>
     );
 }
