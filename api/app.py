194de8cb5a71cc090a69b21b89b957796ab59f4c
Joseph Schwab <52177033+jschwab012@users.noreply.github.com>
2024-04-12 15:07:18 -0400

    Flask running

--- api/app.py
+++ api/app.py
@@ -1,10 +1,14 @@
-from flask import Flask, Response
-
+from flask import Flask, Response, jsonify
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=["GET"])
 def status():
-    return Response({ "status": "running" })
+    return jsonify({
+        "status" : "running"
+    })
+    # return Response({ "status": "running" })
 
 if __name__ == '__main__':
     app.run(port=5001)
