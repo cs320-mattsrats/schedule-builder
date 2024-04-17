fd8903c2a31266c520ad36bf897dd861d3b3a365
Joseph Schwab <52177033+jschwab012@users.noreply.github.com>
2024-04-17 12:32:58 -0400

    Implemented Hot Course grid
    Implemented a grid for all hot courses. Moved course info to course component

--- frontend/src/components/recommendation/Course.tsx
+++ frontend/src/components/recommendation/Course.tsx
@@ -1,16 +1,19 @@
-import { grid } from "@chakra-ui/react";
+import { SimpleGrid, Card, CardBody, CardHeader, Heading, Text, CardFooter, Button } from "@chakra-ui/react";
 
 export default function Course(props:any) {  
     return (
-        <div style={
-            {
-                height: props.height, 
-                width: props.width, 
-                backgroundColor: props.color,
-                }
-            }
-        >
-        </div>
-        // <p style = {{backgroundColor: props.color}}>Hi!</p>
+        <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
+              <Card>
+                <CardHeader>
+                  <Heading size='md'> {props.courseTitle} </Heading>
+                </CardHeader>
+                <CardBody>
+                  <Text> {props.courseDescription} </Text>
+                </CardBody>
+                <CardFooter>
+                  <Button>View here</Button>
+                </CardFooter>
+              </Card>
+            </SimpleGrid>
     );
 }
