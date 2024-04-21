623362dd30bd65bbe5e36483d2e2c17898b80805
Joseph Schwab <52177033+jschwab012@users.noreply.github.com>
2024-04-21 11:40:27 -0400

    Added details to courses
    Gave more detailed information to course cards.

--- frontend/src/app/recommendation/page.tsx
+++ frontend/src/app/recommendation/page.tsx
@@ -2,19 +2,13 @@
 
 import Layout from "@/components/layout/Layout";
 import {
-  Text,
+  Text, SimpleGrid
 } from "@chakra-ui/react";
 
-// [SYL-14] import recommendation page css file and components
 import Course from '@/components/Course'
 import Comment from '@/components/recommendation/Comment'
 import './page.css'
 
-// TODO: Get multiple courses(placeholder) to show up
-// TODO: Center courses
-// TODO: Create comments section header("Student Feedback")
-// TODO: Create comments section
-// TODO: Add placeholder comments
 export default function Recommendation() {
   return (
     <Layout>
@@ -30,7 +24,80 @@ export default function Recommendation() {
             Hot Courses
           </Text>
 
-          <Course height = "300px" width = "250px" color = "red"></Course>
+          <SimpleGrid columns={5} spacing={5}>
+            <Course 
+              courseTitle='CS 220: Programming Methodology'
+              courseDescription="Most iconic class with the most iconic professor Marius Minea."
+              professor='Marius Minea'
+              location1='ILC S131'
+              time1='8:30AM'
+              days1='TTH'
+              type1='LEC'
+              location2='ILC S131'
+              time2='10:10AM'
+              days2='F'
+              type2='DIS'>
+            </Course>
+
+            <Course 
+              courseTitle='CS 230: Computer Systems Principles'
+              courseDescription="Most iconic class with the most iconic professor Joe Chiu."
+              professor='Joe Chiu'
+              location1='HAS 20'
+              time1='2:30PM'
+              days1='TTH'
+              type1='LEC'
+              location2='CS 140'
+              time2='9:05AM'
+              days2='F'
+              type2='DIS'>
+            </Course>
+
+            <Course 
+              courseTitle='CS 250: Introduction to Computation'
+              courseDescription="Most iconic class with the most iconic professor David Mix Barrington."
+              professor='David Mix Barrington'
+              location1='GOES 51'
+              time1='1:25PM'
+              days1='MWF'
+              type1='LEC'
+              location2='LGRC 301A'
+              time2='10:00AM'
+              days2='T'
+              type2='DIS'>
+            </Course>
+
+            <Course 
+              courseTitle='CS 311: Introduction to Algorithms'
+              courseDescription="Most iconic class with the most iconic professor Ghazaleh Parvini."
+              professor='Ghazaleh Parvini'
+              location1='GOES 51'
+              time1='2:30PM'
+              days1='MWF'
+              type1='LEC'
+              location2='LGRC 302A'
+              time2='10:00AM'
+              days2='T'
+              type2='DIS'>
+            </Course>
+
+            <Course 
+              courseTitle='CS 320: Introduction to Software Engineering'
+              courseDescription="Most iconic class with the most iconic professor Matthew Rattigan (WOO HOO :))."
+              professor='Matthew Rattigan'
+              location1='MOR2 127'
+              time1='2:30PM'
+              days1='MWF'
+              type1='LEC'
+              location2='LGRT 141'
+              time2='1:25PM'
+              days2='W'
+              type2='DIS'>
+            </Course>
+            
+          </SimpleGrid>
+          
+
           <Text
             as={"span"}
             bgGradient={
