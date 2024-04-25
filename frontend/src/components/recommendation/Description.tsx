80b4c5a0993322b7c6ee71b691b4c64ac772abd4
mafoley15 <mafoley@umass.edu>
2024-04-24 22:18:17 -0400

    "Edited homepage and course component. Added a modal component to have a course's details about time and location appear in a separate pop-up"

---
+++ frontend/src/components/recommendation/Description.tsx
@@ -0,0 +1,38 @@
+'use client';
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Button,
+    useDisclosure,
+  } from '@chakra-ui/react'
+
+  export default function Description(props: any) {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    return (
+      <>
+        <Button onClick={onOpen}>View Details</Button>
+  
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>{props.courseTitle}</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+                {props.moreDetails}
+            </ModalBody>
+  
+            <ModalFooter>
+              <Button colorScheme='blue' mr={3} onClick={onClose}>
+                Close
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </>
+    )
+  }
